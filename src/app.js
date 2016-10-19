00b81a9049fdfb5c417b7d81cda8e719bc4bcdcc
Nick Johnstone <ncwjohnstone@gmail.com>
2016-10-19 21:26:18 +1300

    Display some code on an SVG canvas

--- src/app.js
+++ src/app.js
@@ -1,12 +1,59 @@
-import {div} from '@cycle/dom';
+import {div, pre, svg, h} from '@cycle/dom';
 import xs from 'xstream';
 
 function App (sources) {
+  const initialState = {};
+
+  const state$ = xs.of(initialState);
+
   return {
-    DOM: xs.of(
-      div('.hello-world', 'Hello world!')
-    )
+    DOM: state$.map(view)
   };
+}
+
+function view () {
+  return (
+    div('.hello-world', [
+      svg({
+        attrs: {
+          width: document.documentElement.clientWidth - 20,
+          height: document.documentElement.clientHeight - 30
+        }
+      }, [
+        renderCodeBlock(App.toString(), {x: 200, y: 200})
+      ])
+    ])
+  )
+}
+
+function renderCodeBlock (code, {x, y}) {
+  const lines = code.split('\n');
+  const LINE_HEIGHT = 16;
+  const CHARACTER_WIDTH = 8;
+  const PADDING = 10;
+
+  const longestLine = Math.max(...lines.map(line => line.length));
+
+  return (
+    h('g', [
+      h('rect', {
+        attrs: {
+          x,
+          y,
+          height:  lines.length * LINE_HEIGHT + PADDING,
+          width: longestLine * CHARACTER_WIDTH + PADDING,
+          stroke: 'skyblue',
+          fill: '#333533'
+        }
+      }),
+
+      h('foreignObject', {attrs: {x: x + 10, y: y + 10}}, [
+        div('.code-wrapper', [
+          pre(code)
+        ])
+      ])
+    ])
+  );
 }
 
 export default App;
---
+++ stories.md
@@ -0,0 +1,5 @@
+
+As a developer
+  I want to have a space to grow my idea into an app
+  I want to be able to write a view
+
